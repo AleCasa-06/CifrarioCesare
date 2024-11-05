6dc00496b4093735ef281bb4813f47c2aeea6290
CasarottoAlessandro <115339459+AleCasa-06@users.noreply.github.com>
2024-11-05 11:37:20 +0100

    Add files via upload

---
+++ HashMD5/Program.cs
@@ -0,0 +1,46 @@
+﻿using HashMD5;
+
+string alfabeto = "abcdefghijklmnopqrstuvwxyz";
+
+
+char[] parola = new char[5];
+
+ha has = new ha();
+
+for (int i = 0; i < alfabeto.Length; i++)
+{
+    parola[0] = alfabeto[i];
+
+    for (int j = 0; j < alfabeto.Length; j++)
+    {
+        parola[1] = alfabeto[j];
+
+        for (int k = 0; k < alfabeto.Length; k++)
+        {
+            parola[2] = alfabeto[k];
+
+            for (int l = 0; l < alfabeto.Length; l++)
+            {
+                parola[3] = alfabeto[l];
+
+                for (int m = 0; m < alfabeto.Length; m++)
+                {
+                    parola[4] = alfabeto[m];
+
+                   
+                    string combinazione = new string(parola);
+
+                    
+                    string hashCombinazione = has.CalcolaHash(combinazione);
+
+                   
+                    if (hashCombinazione == has.getHash())
+                    {
+                        Console.WriteLine($"La password originale è: {combinazione}");
+                        return; 
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ HashMD5/ha.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashMD5
+{
+    public class ha
+    {
+        private string hash; 
+
+        public string getHash()
+        {
+            return hash; 
+        }
+
+        public ha() {
+            hash = "5d41402abc4b2a76b9719d911017c592"; 
+        }   
+
+        public string CalcolaHash(string input)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+                StringBuilder sb = new StringBuilder();
+                foreach (byte b in hashBytes)
+                {
+                    sb.Append(b.ToString("x2"));  // Converte in formato esadecimale
+                }
+                return sb.ToString();
+            }
+        }
+    }
+}
